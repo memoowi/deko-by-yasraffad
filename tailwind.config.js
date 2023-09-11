aa62294dfa93cea40264d0aca77aa7e14783388a
memoowi <megatenlike@gmail.com>
2023-09-12 06:44:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        teko: 'Teko',
+        inter: 'Inter',
+      },
+      fontSize: {
+        navi: '22px',
+        title: '46px',
+      },
+      colors: {
+        review: '#909090',
+      }
+    },
+  },
+  plugins: [],
+}
+
