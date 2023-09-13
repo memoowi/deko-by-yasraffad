1e398312208c368495a8c633d96fd6bb992b932a
memoowi <megatenlike@gmail.com>
2023-09-14 03:34:55 +0700

    update

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
